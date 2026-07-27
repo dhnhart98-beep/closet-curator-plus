e88a10b7c8b0fb0c3229920e9266d3e8965520fd
dhnhart98-beep <dhnhart98@gmail.com>
2026-07-27 02:35:32 -0400

    Add DZ branding and fix iPad photo library upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,16 @@
 
-const CACHE='closet-curator-v1';
+const CACHE='dz-closet-curator-v2';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(e.request,copy));
+      return response;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
