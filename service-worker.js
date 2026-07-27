bbef31402e42aad44be66423dd9f2aa0c0453c95
dhnhart98-beep <dhnhart98@gmail.com>
2026-07-27 03:07:27 -0400

    Add kawaii pink and purple dashboard theme

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
-const CACHE='dz-closet-curator-v2';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='dz-closet-curator-v3';
+const ASSETS=['./','./index.html','./manifest.json','./assets/dz-kawaii-duo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',e=>{
